1c2ff7a6013bdbfc234060cde02608e2318ec5fa
Daniel Torres <danieltolo1@hotmail.com>
2021-12-08 21:53:08 -0500

    inicio de prog de carrousel en pagina de inicio

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
+import Carrusel from 'components/Carrousel';
 
 const Index = () => {
   return (
     <div>
       <div align="center">Bienvenido</div>
-      <div>Index page</div>
-      <div className='bg-green-300 h-96'>
-
-    
-
-
-
-
-
+      <div align="center">
+          <Carrusel></Carrusel>
       </div>
       <div className='bg-green-300 h-96'>Index page</div>
     </div>
