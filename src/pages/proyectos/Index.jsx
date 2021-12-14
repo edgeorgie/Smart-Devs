1da989db537a1c2fd7d2ede0f434b82a774593f1
Edwin Jorge <ed.jorge1122@gmail.com>
2021-12-13 19:09:11 -0500

    index de proyectos corregido

--- src/pages/proyectos/Index.jsx
+++ src/pages/proyectos/Index.jsx
@@ -1,123 +1,326 @@
 import React, { useEffect, useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
-import ButtonLoading from "components/ButtonLoading";
+import { PROYECTOS } from "graphql/proyectos/queries";
+import DropDown from "components/Dropdown";
 import Input from "components/Input";
-import { EDITAR_PERFIL } from "graphql/usuarios/mutations";
+import { Dialog } from "@mui/material";
+import { Enum_EstadoProyecto } from "utils/enums";
+import ButtonLoading from "components/ButtonLoading";
+import { EDITAR_PROYECTO } from "graphql/proyectos/mutations";
 import useFormData from "hooks/useFormData";
-import { uploadFormData } from "utils/uploadFormData";
+import PrivateComponent from "components/PrivateComponent";
+import { Link } from "react-router-dom";
+import { CREAR_INSCRIPCION } from "graphql/inscripciones/mutaciones";
 import { useUser } from "context/userContext";
-import { GET_USUARIO } from "graphql/usuarios/queries";
 import { toast } from "react-toastify";
+import {
+  AccordionStyled,
+  AccordionSummaryStyled,
+  AccordionDetailsStyled
+} from "components/Accordion";
+import { ELIMINAR_OBJETIVO } from "graphql/proyectos/mutations";
+import ReactLoading from "react-loading";
+import { Enum_TipoObjetivo } from "utils/enums";
+import { EDITAR_OBJETIVO } from "graphql/proyectos/mutations";
 
-const Profile = () => {
-  const [editFoto, setEditFoto] = useState(false);
-  const { form, formData, updateFormData } = useFormData();
-  const { userData, setUserData } = useUser();
-
-  const [
-    editarPerfil,
-    { data: dataMutation, error: errorMutation, loading: loadingMutation }
-  ] = useMutation(EDITAR_PERFIL);
-
-  const {
-    data: queryData,
-    loading: queryLoading,
-    error: queryError,
-    refetch
-  } = useQuery(GET_USUARIO, {
-    variables: {
-      _id: userData._id
-    }
-  });
+const IndexProyectos = () => {
+  const { data: queryData, loading } = useQuery(PROYECTOS);
 
   useEffect(() => {
-    if (dataMutation) {
-      console.log("data mutation", dataMutation);
-      setUserData({ ...userData, foto: dataMutation.editarPerfil.foto });
-      toast.success("Perfil modificado con exito");
-      refetch();
-    }
-  }, [dataMutation]);
-
-  useEffect(() => {
-    console.log("ud", queryData);
+    console.log("datos proyecto", queryData);
   }, [queryData]);
 
-  const submitForm = async (e) => {
+  if (loading) return <div>Cargando...</div>;
+
+  if (queryData.Proyectos) {
+    return (
+      <div className="p-10 flex flex-col">
+        <div className="flex w-full items-center justify-center">
+          <h1 className="text-2xl font-bold text-gray-900">
+            Lista de Proyectos
+          </h1>
+        </div>
+        <PrivateComponent roleList={["ADMINISTRADOR", "LIDER"]}>
+          <div className="my-2 self-end">
+            <button className="bg-indigo-500 text-gray-50 p-2 rounded-lg shadow-lg hover:bg-indigo-400">
+              <Link to="/proyectos/nuevo">Crear nuevo proyecto</Link>
+            </button>
+          </div>
+        </PrivateComponent>
+        {queryData.Proyectos.map((proyecto) => {
+          return <AccordionProyecto proyecto={proyecto} />;
+        })}
+      </div>
+    );
+  }
+
+  return <></>;
+};
+
+const AccordionProyecto = ({ proyecto }) => {
+  const [showDialog, setShowDialog] = useState(false);
+  return (
+    <>
+      <AccordionStyled>
+        <AccordionSummaryStyled
+          expandIcon={<i className="fas fa-chevron-down" />}
+        >
+          <div className="flex w-full justify-between">
+            <div className="uppercase font-bold text-gray-100 ">
+              {proyecto.nombre} - {proyecto.estado}
+            </div>
+          </div>
+        </AccordionSummaryStyled>
+        <AccordionDetailsStyled>
+          <PrivateComponent roleList={["ADMINISTRADOR"]}>
+            <i
+              className="mx-4 fas fa-pen text-yellow-600 hover:text-yellow-400"
+              onClick={() => {
+                setShowDialog(true);
+              }}
+            />
+          </PrivateComponent>
+          <PrivateComponent roleList={["ESTUDIANTE"]}>
+            <InscripcionProyecto
+              idProyecto={proyecto._id}
+              estado={proyecto.estado}
+              inscripciones={proyecto.inscripciones}
+            />
+          </PrivateComponent>
+          <div>Liderado Por: {proyecto.lider.correo}</div>
+          <div className="flex">
+            {proyecto.objetivos.map((objetivo, index) => {
+              return (
+                <Objetivo
+                  index={index}
+                  _id={objetivo._id}
+                  idProyecto={proyecto._id}
+                  tipo={objetivo.tipo}
+                  descripcion={objetivo.descripcion}
+                />
+              );
+            })}
+          </div>
+        </AccordionDetailsStyled>
+      </AccordionStyled>
+      <Dialog
+        open={showDialog}
+        onClose={() => {
+          setShowDialog(false);
+        }}
+      >
+        <FormEditProyecto _id={proyecto._id} />
+      </Dialog>
+    </>
+  );
+};
+
+const FormEditProyecto = ({ _id }) => {
+  const { form, formData, updateFormData } = useFormData();
+  const [editarProyecto, { data: dataMutation, loading, error }] =
+    useMutation(EDITAR_PROYECTO);
+
+  const submitForm = (e) => {
     e.preventDefault();
-
-    const formUploaded = await uploadFormData(formData);
-
-    console.log("form cargado", formUploaded);
-
-    editarPerfil({
+    editarProyecto({
       variables: {
-        _id: userData._id,
-        campos: formUploaded
+        _id,
+        campos: formData
       }
     });
   };
 
-  if (queryLoading) return <div>Loading...</div>;
+  useEffect(() => {
+    console.log("data mutation", dataMutation);
+  }, [dataMutation]);
 
   return (
-    <div className="p-10 flex flex-col items-center justify-center w-full">
-      <h1 className="font-bold text-2xl text-gray-900">Perfil del usuario</h1>
+    <div className="p-4">
+      <h1 className="font-bold">Modificar Estado del Proyecto</h1>
+      <form
+        ref={form}
+        onChange={updateFormData}
+        onSubmit={submitForm}
+        className="flex flex-col items-center"
+      >
+        <DropDown
+          label="Estado del Proyecto"
+          name="estado"
+          options={Enum_EstadoProyecto}
+        />
+        <ButtonLoading disabled={false} loading={loading} text="Confirmar" />
+      </form>
+    </div>
+  );
+};
+
+const Objetivo = ({ index, _id, idProyecto, tipo, descripcion }) => {
+  const [showEditDialog, setShowEditDialog] = useState(false);
+  const [
+    eliminarObjetivo,
+    { data: dataMutationEliminar, loading: eliminarLoading }
+  ] = useMutation(ELIMINAR_OBJETIVO, {
+    refetchQueries: [{ query: PROYECTOS }]
+  });
+
+  useEffect(() => {
+    console.log("eliminar objetivo:", dataMutationEliminar);
+    if (dataMutationEliminar) {
+      toast.success("objetivo eliminado satisfactoriamente");
+    }
+  }, [dataMutationEliminar]);
+
+  const ejecutarEliminacion = () => {
+    eliminarObjetivo({ variables: { idProyecto, idObjetivo: _id } });
+  };
+
+  if (eliminarLoading)
+    return (
+      <ReactLoading
+        data-testid="loading-in-button"
+        type="spin"
+        height={100}
+        width={100}
+      />
+    );
+  return (
+    <div className="mx-5 my-4 bg-gray-50 p-8 rounded-lg flex flex-col items-center justify-center shadow-xl">
+      <div className="text-lg font-bold">{tipo}</div>
+      <div>{descripcion}</div>
+      <PrivateComponent roleList={["ADMINISTRADOR", "LIDER"]}>
+        <div className="flex my-2">
+          <i
+            onClick={() => setShowEditDialog(true)}
+            className="fas fa-pen mx-2 text-yellow-500 hover:text-yellow-200 cursor-pointer"
+          />
+          <i
+            onClick={ejecutarEliminacion}
+            className="fas fa-trash mx-2 text-red-500 hover:text-red-200 cursor-pointer"
+          />
+        </div>
+        <Dialog open={showEditDialog} onClose={() => setShowEditDialog(false)}>
+          <EditarObjetivo
+            descripcion={descripcion}
+            tipo={tipo}
+            index={index}
+            idProyecto={idProyecto}
+            setShowEditDialog={setShowEditDialog}
+          />
+        </Dialog>
+      </PrivateComponent>
+    </div>
+  );
+};
+
+const EditarObjetivo = ({
+  descripcion,
+  tipo,
+  index,
+  idProyecto,
+  setShowEditDialog
+}) => {
+  const { form, formData, updateFormData } = useFormData();
+
+  const [editarObjetivo, { data: dataMutation, loading }] = useMutation(
+    EDITAR_OBJETIVO,
+    {
+      refetchQueries: [{ query: PROYECTOS }]
+    }
+  );
+
+  useEffect(() => {
+    if (dataMutation) {
+      toast.success("Objetivo editado con exito");
+      setShowEditDialog(false);
+    }
+  }, [dataMutation, setShowEditDialog]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    editarObjetivo({
+      variables: {
+        idProyecto,
+        indexObjetivo: index,
+        campos: formData
+      }
+    }).catch((e) => {
+      console.log(e);
+      toast.error("Error editando el objetivo");
+    });
+  };
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-gray-900">Editar Objetivo</h1>
       <form ref={form} onChange={updateFormData} onSubmit={submitForm}>
-        <Input
-          defaultValue={queryData.Usuario.nombre}
-          label="Nombre"
-          name="nombre"
-          type="text"
+        <DropDown
+          label="Tipo de Objetivo"
+          name="tipo"
           required={true}
+          options={Enum_TipoObjetivo}
+          defaultValue={tipo}
         />
         <Input
-          defaultValue={queryData.Usuario.apellido}
-          label="Apellido"
-          name="apellido"
-          type="text"
+          label="Descripcion del objetivo"
+          name="descripcion"
           required={true}
+          defaultValue={descripcion}
         />
-        <Input
-          defaultValue={queryData.Usuario.identificacion}
-          label="Identificación"
-          name="identificacion"
-          type="text"
-          required={true}
-        />
-        {queryData.Usuario.foto && !editFoto ? (
-          <div className="flex flex-col items-center">
-            <img
-              className="h-32"
-              src={queryData.Usuario.foto}
-              alt="Foto Usuario"
-            />
-            <button
-              onClick={() => setEditFoto(true)}
-              className="bg-indigo-300 p-1 my-2 rounded-md text-white"
-            >
-              Cambiar imagen
-            </button>
-          </div>
-        ) : (
-          <div>
-            <Input label="Foto" name="foto" type="file" required={true} />
-            <button
-              onClick={() => setEditFoto(false)}
-              className="bg-indigo-300 p-1 my-2 rounded-md text-white"
-            >
-              Cancelar
-            </button>
-          </div>
-        )}
         <ButtonLoading
           text="Confirmar"
-          loading={loadingMutation}
-          disabled={false}
+          disabled={Object.keys(formData).length === 0}
+          loading={loading}
         />
       </form>
     </div>
   );
 };
 
-export default Profile;
+const InscripcionProyecto = ({ idProyecto, estado, inscripciones }) => {
+  const [estadoInscripcion, setEstadoInscripcion] = useState("");
+  const [crearInscripcion, { data, loading, error }] =
+    useMutation(CREAR_INSCRIPCION);
+  const { userData } = useUser();
+
+  useEffect(() => {
+    if (userData && inscripciones) {
+      const flt = inscripciones.filter(
+        (el) => el.estudiante._id === userData._id
+      );
+      if (flt.length > 0) {
+        setEstadoInscripcion(flt[0].estado);
+      }
+    }
+  }, [userData, inscripciones]);
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      toast.success("inscripcion creada con exito");
+    }
+  }, [data]);
+
+  const confirmarInscripcion = () => {
+    crearInscripcion({
+      variables: { proyecto: idProyecto, estudiante: userData._id }
+    });
+  };
+
+  return (
+    <>
+      {estadoInscripcion !== "" ? (
+        <span>
+          Ya estas inscrito en este proyecto y el estado es {estadoInscripcion}
+        </span>
+      ) : (
+        <ButtonLoading
+          onClick={() => confirmarInscripcion()}
+          disabled={estado === "INACTIVO"}
+          loading={loading}
+          text="Inscribirme en este proyecto"
+        />
+      )}
+    </>
+  );
+};
+
+export default IndexProyectos;
