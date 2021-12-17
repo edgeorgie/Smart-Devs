90ab943583090523ae5defe80f535f77da4d45ac
Luz Aida Buitrago Rodriguez <aidabuitrago77@gmail.com>
2021-12-17 17:49:51 -0500

    en L53 se cambia el titulo de la aplicaciÃon por uno mas corto con las iniciales del anterior que SIGPI

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -50,7 +50,7 @@ const Logo = () => (
   <div className="py-3 w-full flex flex-col items-center justify-center">
     <img src="logo.png" alt="Logo" className="h-16" />
     <span className="my-2 text-xl font-bold text-center">
-      Sistema de Información de Gestión de Proyectos de Investigación
+      SIGPI
     </span>
   </div>
 );
