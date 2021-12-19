694c21fc5a800123452f1a2d5f6c79f4ef8ebef6
Edwin Jorge <ed.jorge1122@gmail.com>
2021-12-18 20:05:17 -0500

    pull de main y solucion de conflictos en hook/useFormData

--- src/hooks/useFormData.js
+++ src/hooks/useFormData.js
@@ -7,13 +7,8 @@ const useFormData = (initial) => {
     const fd = new FormData(form.current);
     const obj = {};
     fd.forEach((value, key) => {
-<<<<<<< HEAD
-      if (key.includes("nested")) {
-        const [p0, p1, p2, p3] = key.split("||");
-=======
       if (key.includes('nested')) {
         const [p1, p2, p3] = key.split('||');
->>>>>>> 5bdeae6148b79786891269083504ca2b9bd8d04c
         if (Object.keys(obj).includes(p1)) {
           if (Object.keys(obj[p1]).includes(p2)) {
             obj[p1][p2][p3] = value;
